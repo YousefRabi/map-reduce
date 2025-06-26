801910b480aca150207858c6e8c6518213e0b5c8
Yousef Rabi <yousofrabi@gmail.com>
2025-06-26 14:14:07 +1000

    move to a communication based method instead of a naive sleep

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -22,66 +22,91 @@ type Coordinator struct {
 	reducePending    map[int]bool
 	reduceInProgress map[int]bool
 	reduceCompleted  map[int]bool
-	nReduce          int
-	mu               sync.Mutex
+	// Channels for coordinating task availability
+	mapTaskAvailable    chan struct{}
+	reduceTaskAvailable chan struct{}
+	jobDone             chan struct{}
+	nReduce             int
+	mu                  sync.Mutex
 }
 
 // Worker uses this to ask for a task.
 func (c *Coordinator) GetTask(args *EmptyArgs, reply *Task) error {
-	// If no map tasks are pending and some are in progress, keep waiting
-	// in case some time out and become pending again
-	for len(c.mapPending) == 0 &&
-		len(c.mapInProgress) > 0 {
-		time.Sleep(time.Millisecond)
-	}
+	for {
+		if c.Done() {
+			reply.TaskType = EXIT
+			return nil
+		}
 
-	if c.Done() {
-		reply.TaskType = EXIT
-		return nil
-	}
+		// Try get a map task
+		c.mu.Lock()
+		for taskNum, filename := range c.mapPending {
+			reply.Filename = filename
+			reply.TaskType = MAP
+			reply.TaskNum = taskNum
+			c.mapInProgress[taskNum] = filename
+			delete(c.mapPending, taskNum)
+			go c.track(*reply)
+			c.mu.Unlock()
+			return nil
+		}
 
-	c.mu.Lock()
-	for taskNum, filename := range c.mapPending {
-		reply.Filename = filename
-		reply.TaskType = MAP
-		reply.TaskNum = taskNum
-		c.mapInProgress[taskNum] = filename
-		delete(c.mapPending, taskNum)
-		go c.track(*reply)
+		allMapsDone := len(c.mapInProgress) == 0
 		c.mu.Unlock()
-		return nil
-	}
-	c.mu.Unlock()
 
-	// If no reduce tasks are pending and some are in progress, keep waiting
-	// in case some time out and become pending again
-	for len(c.reducePending) == 0 &&
-		len(c.reduceInProgress) > 0 {
-		time.Sleep(time.Millisecond)
-	}
+		if allMapsDone {
+			// Try get a reduce task
+			c.mu.Lock()
+			for reduceTask := range c.reducePending {
+				reply.TaskType = REDUCE
+				reply.TaskNum = reduceTask
+				c.reduceInProgress[reduceTask] = true
+				delete(c.reducePending, reduceTask)
+				go c.track(*reply)
+				c.mu.Unlock()
+				return nil
+			}
+			c.mu.Unlock()
 
-	c.mu.Lock()
-	for reduceTask := range c.reducePending {
-		reply.TaskType = REDUCE
-		reply.TaskNum = reduceTask
-		c.reduceInProgress[reduceTask] = true
-		delete(c.reducePending, reduceTask)
-		go c.track(*reply)
-		c.mu.Unlock()
-		return nil
+			// No reduce tasks available, wait for notification
+			select {
+			case <-c.reduceTaskAvailable:
+				continue
+			case <-c.jobDone:
+				reply.TaskType = EXIT
+				return nil
+			}
+		} else {
+			// Maps still in progress, wait for notification
+			select {
+			case <-c.mapTaskAvailable:
+				continue
+			case <-c.reduceTaskAvailable:
+				continue
+			case <-c.jobDone:
+				reply.TaskType = EXIT
+				return nil
+			}
+		}
+
 	}
-	c.mu.Unlock()
-	return nil
 }
 
 // Worker uses this to mark a map task as completed.
 func (c *Coordinator) CompleteMapTask(args *Task, reply *EmptyArgs) error {
 	taskNum := args.TaskNum
+	c.mu.Lock()
 	if _, ok := c.mapInProgress[taskNum]; !ok {
 		return fmt.Errorf("worker said map taskNum %v completed when it was not even in progress", taskNum)
 	}
-	c.mu.Lock()
 	delete(c.mapInProgress, taskNum)
+
+	if len(c.mapPending) == 0 && len(c.mapInProgress) == 0 {
+		select {
+		case c.reduceTaskAvailable <- struct{}{}:
+		default: // nonblocking
+		}
+	}
 	c.mu.Unlock()
 	return nil
 }
@@ -89,13 +114,17 @@ func (c *Coordinator) CompleteMapTask(args *Task, reply *EmptyArgs) error {
 // Worker uses this to mark a reduce task as completed.
 func (c *Coordinator) CompleteReduceTask(args *Task, reply *EmptyArgs) error {
 	taskNum := args.TaskNum
+	c.mu.Lock()
 	if _, ok := c.reduceInProgress[taskNum]; !ok {
 		return fmt.Errorf("worker said %v taskNum reduce completed when it was not even in progress", taskNum)
 	}
-	c.mu.Lock()
 	c.reduceCompleted[taskNum] = true
 	delete(c.reduceInProgress, taskNum)
 	c.mu.Unlock()
+
+	if c.Done() {
+		close(c.jobDone)
+	}
 	return nil
 }
 
@@ -106,21 +135,31 @@ func (c *Coordinator) track(task Task) error {
 	time.Sleep(10 * time.Second)
 	switch task.TaskType {
 	case MAP:
+		c.mu.Lock()
 		if _, ok := c.mapInProgress[task.TaskNum]; ok {
 			fmt.Printf("%v task num %v %v took longer than 10s, rescheduling it...\n", task.TaskType, task.TaskNum, task.Filename)
-			c.mu.Lock()
 			c.mapPending[task.TaskNum] = task.Filename
 			delete(c.mapInProgress, task.TaskNum)
-			c.mu.Unlock()
+
+			select {
+			case c.mapTaskAvailable <- struct{}{}:
+			default:
+			}
 		}
+		c.mu.Unlock()
 	case REDUCE:
+		c.mu.Lock()
 		if _, ok := c.reduceInProgress[task.TaskNum]; ok {
 			fmt.Printf("%v task num %v %v took longer than 10s, rescheduling it...\n", task.TaskType, task.TaskNum, task.Filename)
-			c.mu.Lock()
 			c.reducePending[task.TaskNum] = true
 			delete(c.reduceInProgress, task.TaskNum)
-			c.mu.Unlock()
+
+			select {
+			case c.reduceTaskAvailable <- struct{}{}:
+			default:
+			}
 		}
+		c.mu.Unlock()
 	}
 	return nil
 }
@@ -171,6 +210,9 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c.reduceInProgress = make(map[int]bool)
 	c.reduceCompleted = make(map[int]bool)
 	c.nReduce = nReduce
+	c.mapTaskAvailable = make(chan struct{}, 1)
+	c.reduceTaskAvailable = make(chan struct{}, 1)
+	c.jobDone = make(chan struct{})
 
 	for i, file := range files {
 		c.mapPending[i] = file
